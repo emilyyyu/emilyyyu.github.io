4c9a4d1e876490fd5955d10f0bde41e1864fd488
emilyyyu <yzqemily@hotmail.com>
2025-10-21 10:45:54 +0000

    Deploying to gh-pages from @ emilyyyu/emilyyyu.github.io@2fa9a14e44034520068ae99fb2ccffcd449ed07d 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -16,13 +16,6 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/publications/";
           },
-        },{id: "nav-projects",
-          title: "projects",
-          description: "This page is under construction.",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/projects/";
-          },
         },{id: "nav-cv",
           title: "cv",
           description: "",
