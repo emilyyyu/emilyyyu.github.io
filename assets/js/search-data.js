732c58b467db1d4ef92e9371cfce9d9e9a45d1df
emilyyyu <yzqemily@hotmail.com>
2025-10-21 10:57:33 +0000

    Deploying to gh-pages from @ emilyyyu/emilyyyu.github.io@61b3db5cc9bacd66249fa68d1f937e4a57478bbb 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -30,6 +30,13 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/teaching/";
           },
+        },{id: "nav-veritas-leiden",
+          title: "Veritas@Leiden",
+          description: "",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/teaching/";
+          },
         },{id: "books-the-godfather",
           title: 'The Godfather',
           description: "",
